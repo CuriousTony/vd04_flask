5e226ec8593480c53c8f2b7d95593770400ff5fa
CuriousTony <aszdorovtsev@yandex.ru>
2024-11-12 16:10:01 +0300

    Job's done.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/")
-def hello_world():
-    return ...
+def browse_index():
+    return render_template("index.html")
+
+
+@app.route("/blog/")
+def browse_blog():
+    return render_template("blog.html")
+
+
+@app.route("/contacts/")
+def browse_contacts():
+    return render_template("contacts.html")
+
 
 if __name__ == "__main__":
     app.run()
